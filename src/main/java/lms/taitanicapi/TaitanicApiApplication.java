02d1b1544495c6faa5b8f78b75184511428ebfc4
LMS <mslee6524@naver.com>
2023-08-07 10:29:41 +0900

    first commit

---
+++ src/main/java/lms/taitanicapi/TaitanicApiApplication.java
@@ -0,0 +1,13 @@
+package lms.taitanicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaitanicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaitanicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lms/taitanicapi/TaitanicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lms.taitanicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaitanicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
